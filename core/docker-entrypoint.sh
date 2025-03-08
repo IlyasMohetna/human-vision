c87c057cbc5c590494854e7752adc16223fa8ea8
Ilyas MOHETNA <ilyas.mohetna@hotmail.com>
2025-03-08 18:43:11 +0100

    adding default databse configuration

--- core/docker-entrypoint.sh
+++ core/docker-entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 if [ ! -d vendor ]; then
   echo "Vendor directory not found. Installing Composer dependencies..."
   composer install
+  cp .env.example .env
+  php artisan key:generate
+  php artisan migrate
+  php artisan db:seed
 fi
 
 exec "$@"
