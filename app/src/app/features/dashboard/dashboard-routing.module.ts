d33e3723796261591ca8024b753c36634e4b4ae6
Ilyas MOHETNA <ilyas.mohetna@hotmail.com>
2025-03-15 05:09:20 +0100

    handle conditional layout showing

--- app/src/app/features/dashboard/dashboard-routing.module.ts
+++ app/src/app/features/dashboard/dashboard-routing.module.ts
@@ -3,15 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
 import { HomeComponent } from './user/home/home.component';
 import { SettingsComponent } from './user/settings/settings.component';
+import { UserRoutingModule } from './user/user-routing.module';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardLayoutComponent,
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'settings', component: SettingsComponent },
+      {
+        path: '',
+        loadChildren: () =>
+          import('./user/user-routing.module').then((m) => m.UserRoutingModule),
+      },
     ],
   },
 ];
--- app/src/app/features/dashboard/user/user-routing.module.ts
+++ app/src/app/features/dashboard/user/user-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const USER_MENU = [
   { name: 'Analytics', route: '/user', icon: 'home' },
@@ -8,7 +10,14 @@ export const USER_MENU = [
   { name: 'Settings', route: '/user/settings', icon: 'user-cog' },
 ];
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, data: { showLayout: true } },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    data: { showLayout: false },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
