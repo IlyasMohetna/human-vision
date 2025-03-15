afaba7507646001a4a82bffaacf914cc44df1740
Ilyas MOHETNA <ilyas.mohetna@hotmail.com>
2025-03-15 05:29:35 +0100

    start creating the studio page

--- app/src/app/features/dashboard/user/user-routing.module.ts
+++ app/src/app/features/dashboard/user/user-routing.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { StudioComponent } from './studio/studio.component';
 
 export const USER_MENU = [
   { name: 'Analytics', route: '/user', icon: 'home' },
-  { name: 'Studio', route: '/user/orders', icon: 'shopping-cart' },
+  { name: 'Studio', route: '/dashboard/studio', icon: 'shopping-cart' },
   { name: 'Reviews', route: '/user/profile', icon: 'user' },
   { name: 'Settings', route: '/user/settings', icon: 'user-cog' },
 ];
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, data: { showLayout: true } },
+  { path: 'analytics', component: HomeComponent },
+  { path: 'studio', component: StudioComponent, data: { showLayout: false } },
   {
     path: 'settings',
     component: SettingsComponent,
-    data: { showLayout: false },
   },
 ];
 
