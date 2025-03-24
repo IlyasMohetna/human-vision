c0e2bf5eaf3ac19e10bb9480bc00d95255a86f44
Ilyas MOHETNA <ilyas.mohetna@hotmail.com>
2025-03-24 18:16:13 +0100

    create a first import page

--- app/src/app/features/dashboard/admin/admin-routing.module.ts
+++ app/src/app/features/dashboard/admin/admin-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ImportComponent } from './import/import.component';
 
 export const ADMIN_MENU = [
   { name: 'Dashboard', route: 'admin/dashboard', icon: 'dashboard' },
+  { name: 'Import Data', route: 'admin/import', icon: 'download' },
 ];
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: HomeComponent,
+  },
+  {
+    path: 'import',
+    component: ImportComponent,
   },
 ];
 
