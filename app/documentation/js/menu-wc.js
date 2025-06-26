50a032fbda6e014c79050e8051ff282fb53d31fa
Ilyas MOHETNA <ilyas.mohetna@hotmail.com>
2025-06-26 07:33:45 +0200

    add route to manual import from local storage folder

--- app/documentation/js/menu-wc.js
+++ app/documentation/js/menu-wc.js
@@ -66,6 +66,20 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                             <li class="link">
                                 <a href="modules/DashboardModule.html" data-type="entity-link" >DashboardModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ?
+                                            'data-bs-target="#components-links-module-DashboardModule-cd634648448c16c4a90114951f4ee2a2a395b4668631d172c0e214c9de14a33210cb887420122b7d45aa4b8d8603dffc52c8f807a79bfe0e0de18e8624f71ea4"' : 'data-bs-target="#xs-components-links-module-DashboardModule-cd634648448c16c4a90114951f4ee2a2a395b4668631d172c0e214c9de14a33210cb887420122b7d45aa4b8d8603dffc52c8f807a79bfe0e0de18e8624f71ea4"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-DashboardModule-cd634648448c16c4a90114951f4ee2a2a395b4668631d172c0e214c9de14a33210cb887420122b7d45aa4b8d8603dffc52c8f807a79bfe0e0de18e8624f71ea4"' :
+                                            'id="xs-components-links-module-DashboardModule-cd634648448c16c4a90114951f4ee2a2a395b4668631d172c0e214c9de14a33210cb887420122b7d45aa4b8d8603dffc52c8f807a79bfe0e0de18e8624f71ea4"' }>
+                                            <li class="link">
+                                                <a href="components/HomeComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >HomeComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
                             </li>
                             <li class="link">
                                 <a href="modules/DashboardRoutingModule.html" data-type="entity-link" >DashboardRoutingModule</a>
@@ -87,7 +101,16 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
                             <li class="link">
+                                <a href="components/AnnotationCategoryComponent.html" data-type="entity-link" >AnnotationCategoryComponent</a>
+                            </li>
+                            <li class="link">
                                 <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/BadgeComponent.html" data-type="entity-link" >BadgeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CommentModalComponent.html" data-type="entity-link" >CommentModalComponent</a>
                             </li>
                             <li class="link">
                                 <a href="components/ControlBarComponent.html" data-type="entity-link" >ControlBarComponent</a>
@@ -99,10 +122,31 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="components/DashboardLayoutComponent.html" data-type="entity-link" >DashboardLayoutComponent</a>
                             </li>
                             <li class="link">
+                                <a href="components/DatasetComponent.html" data-type="entity-link" >DatasetComponent</a>
+                            </li>
+                            <li class="link">
                                 <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
                             </li>
                             <li class="link">
+                                <a href="components/HomeComponent-1.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ImportComponent.html" data-type="entity-link" >ImportComponent</a>
+                            </li>
+                            <li class="link">
                                 <a href="components/LeftSideBarComponent.html" data-type="entity-link" >LeftSideBarComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoadingOverlayComponent.html" data-type="entity-link" >LoadingOverlayComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MapComponent.html" data-type="entity-link" >MapComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MetadataComponent.html" data-type="entity-link" >MetadataComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RightSideBarComponent.html" data-type="entity-link" >RightSideBarComponent</a>
                             </li>
                             <li class="link">
                                 <a href="components/SettingsComponent.html" data-type="entity-link" >SettingsComponent</a>
@@ -112,6 +156,18 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                             <li class="link">
                                 <a href="components/StudioComponent.html" data-type="entity-link" >StudioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/TrafficSignsComponent.html" data-type="entity-link" >TrafficSignsComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/VariantsComponent.html" data-type="entity-link" >VariantsComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/VehicleComponent.html" data-type="entity-link" >VehicleComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/WeatherComponent.html" data-type="entity-link" >WeatherComponent</a>
                             </li>
                         </ul>
                     </li>
@@ -124,7 +180,19 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </div>
                             <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
                                 <li class="link">
+                                    <a href="injectables/AnnotationService.html" data-type="entity-link" >AnnotationService</a>
+                                </li>
+                                <li class="link">
                                     <a href="injectables/AuthService.html" data-type="entity-link" >AuthService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/KeyboardService.html" data-type="entity-link" >KeyboardService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/ModuleLoaderService.html" data-type="entity-link" >ModuleLoaderService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/PolygonDataService.html" data-type="entity-link" >PolygonDataService</a>
                                 </li>
                                 <li class="link">
                                     <a href="injectables/ThemeService.html" data-type="entity-link" >ThemeService</a>
@@ -148,6 +216,12 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <li class="link">
                                 <a href="guards/GuestGuard.html" data-type="entity-link" >GuestGuard</a>
                             </li>
+                            <li class="link">
+                                <a href="guards/RoleGuard.html" data-type="entity-link" >RoleGuard</a>
+                            </li>
+                            <li class="link">
+                                <a href="guards/UserResolver.html" data-type="entity-link" >UserResolver</a>
+                            </li>
                         </ul>
                     </li>
                     <li class="chapter">
@@ -159,7 +233,28 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
                             <li class="link">
+                                <a href="interfaces/BoundingBox.html" data-type="entity-link" >BoundingBox</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Dataset.html" data-type="entity-link" >Dataset</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/PaginationInfo.html" data-type="entity-link" >PaginationInfo</a>
+                            </li>
+                            <li class="link">
                                 <a href="interfaces/PolygonData.html" data-type="entity-link" >PolygonData</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/PolygonDataMap.html" data-type="entity-link" >PolygonDataMap</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/PolygonResponse.html" data-type="entity-link" >PolygonResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/TrafficSignResponse.html" data-type="entity-link" >TrafficSignResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/TrafficSignResult.html" data-type="entity-link" >TrafficSignResult</a>
                             </li>
                             <li class="link">
                                 <a href="interfaces/User.html" data-type="entity-link" >User</a>
